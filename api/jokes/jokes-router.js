322179ad27681f4a3b67ec7913b70ed0c576cf22
Travis Snapp <traviswsnapp@outlook.com>
2024-01-05 01:17:28 -0500

    updated restrict so it runs through server

--- api/jokes/jokes-router.js
+++ api/jokes/jokes-router.js
@@ -1,9 +1,6 @@
 // do not make changes to this file
 const router = require("express").Router();
 const jokes = require("./jokes-data");
-const restrictedMiddleware = require("../middleware/restricted");
-
-router.use(restrictedMiddleware);
 
 router.get("/", (req, res) => {
   res.status(200).json(jokes);
